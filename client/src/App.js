fd4a919318c3a8110ca663cfaf1f4cad0b876d67
“Pankaj” <ramchandanipankaj787@gmail.com>
2022-12-10 08:35:19 +0530

    styling and redux boiler plate

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,7 @@
+import { CssBaseline, ThemeProvider } from "@mui/material";
+
 function App() {
-  return (
-    <div className="app">
-      
-    </div>
-  );
+  return <div className="app"></div>;
 }
 
 export default App;
